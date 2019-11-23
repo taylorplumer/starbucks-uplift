ab31470ae5117245f27f2378435ecadaf6e7e53a
Taylor Plumer <45341982+taylorplumer@users.noreply.github.com>
2019-11-23 13:34:04 -0800

    Update directory where return_figures is located
    Update the directory from 'wrangling_scripts' to 'data' directory

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,7 +1,7 @@
 from myapp import app
 import json, plotly
 from flask import render_template
-from wrangling_scripts.visualization import return_figures
+from data.visualization import return_figures
 
 @app.route('/')
 @app.route('/index')
